b400f23bbe3f4678246fdbbd451bf722484a0258
= <jules.grivot.pelisson@gmail.com>
2024-11-04 17:38:19 +0100

    premier Game loop

--- frames/Jeu.py
+++ frames/Jeu.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk,Canvas
-  
+import time
+
 LARGEFONT = "Verdana"
 
 class Jeu(tk.Frame):
@@ -20,6 +21,18 @@ class Jeu(tk.Frame):
         button3 = ttk.Button(self, text ="QUIT", 
                              command = lambda : controller.destroy())
         button3.grid(row = 5, column = 2, padx = 10, pady = 10)
+        
+        self.gameLoop()
+        
+        
+    def updatewindow(self, fps=60):
+        time.sleep(1/fps)
+        self.update()
+        
+    def gameLoop(self):
+        while True:
+            
+            self.updatewindow()
         
         
         
