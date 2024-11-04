2de3a7ec8467c60049114bd9579879dc512a5f2b
CSIletonk <raphael.dziopa@gmail.com>
2024-11-04 16:40:28 +0100

    Menu made main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import ttk
   
  
-LARGEFONT =("Verdana", 35)
+LARGEFONT = "Verdana"
   
 class tkinterApp(tk.Tk):
      
@@ -21,7 +21,7 @@ class tkinterApp(tk.Tk):
         container.grid_columnconfigure(0, weight = 1)
   
   
-        allFrames = [StartPage, Page1, Page2]
+        allFrames = [StartPage, Jeu, Menu]
   
         # initializing frames to an empty array
         self.frames = {}  
@@ -53,115 +53,54 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller): 
         tk.Frame.__init__(self, parent)
          
-        # label of frame Layout 2
-        label = ttk.Label(self, text ="Startpage", font = LARGEFONT)
-         
-        # putting the grid in its place by using
-        # grid
-        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+        label = ttk.Label(self, text ="Welcome to", font = (LARGEFONT,15))
+        label = ttk.Label(self, text ="SPACE INVADERS the Game !", font = (LARGEFONT,25))
+        label.grid(row = 1, column = 1, padx = 10, pady = 10) 
   
-        button1 = ttk.Button(self, text ="Page 1",
-        command = lambda : controller.show_frame(Page1))
-     
-        # putting the button in its place by
-        # using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+        button1 = ttk.Button(self, text ="Play",
+        command = lambda : controller.show_frame(Jeu))
+        button1.grid(row = 2, column = 2, padx = 10, pady = 10)
   
-        ## button to show frame 2 with text layout2
-        button2 = ttk.Button(self, text ="Page 2",
-        command = lambda : controller.show_frame(Page2))
-     
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+        button2 = ttk.Button(self, text ="Menu",
+        command = lambda : controller.show_frame(Menu))
+        button2.grid(row = 3, column = 2, padx = 10, pady = 10)
+
+        button3 = ttk.Button(self, text ="QUIT", 
+                             command = lambda : controller.destroy)
+        button3.grid(row = 5, column = 2, padx = 10, pady = 10)
   
           
   
-  
-# second window frame page1 
-class Page1(tk.Frame):
-     
+class Jeu(tk.Frame):
     def __init__(self, parent, controller):
-         
+
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Page 1", font = LARGEFONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+        label = ttk.Label(self, text ="Play", font = (LARGEFONT,15))
+        label.grid(row = 1, column = 1, padx = 10, pady = 10)
+        
+        button1 = ttk.Button(self, text ="Menu",
+        command = lambda : controller.show_frame(Menu))
+        button1.grid(row = 2, column = 2, padx = 10, pady = 10)
+
+        button3 = ttk.Button(self, text ="QUIT", 
+                             command = lambda : controller.destroy)
+        button3.grid(row = 5, column = 2, padx = 10, pady = 10)
   
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text ="StartPage",
-                            command = lambda : controller.show_frame(StartPage))
-     
-        # putting the button in its place 
-        # by using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-  
-        # button to show frame 2 with text
-        # layout2
-        button2 = ttk.Button(self, text ="Page 2",
-                            command = lambda : controller.show_frame(Page2))
-     
-        # putting the button in its place by 
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
-  
-  
-  
-  
-# third window frame page2
-class Page2(tk.Frame): 
+class Menu(tk.Frame): 
     def __init__(self, parent, controller):
+        
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Page 2", font = LARGEFONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
-  
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text ="Page 1",
-                            command = lambda : controller.show_frame(Page1))
-     
-        # putting the button in its place by 
-        # using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-  
-        # button to show frame 3 with text
-        # layout3
-        button2 = ttk.Button(self, text ="Startpage",
-                            command = lambda : controller.show_frame(StartPage))
-     
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
-  
-  
+        label = ttk.Label(self, text ="Menu", font = (LARGEFONT,15))
+        label.grid(row = 1, column = 1, padx = 10, pady = 10)
+        
+        button1 = ttk.Button(self, text ="Play",
+        command = lambda : controller.show_frame(Jeu))
+        button1.grid(row = 2, column = 2, padx = 10, pady = 10)
+
+        button3 = ttk.Button(self, text ="QUIT", 
+                             command = lambda : controller.destroy)
+        button3.grid(row = 5, column = 2, padx = 10, pady = 10)
+
 # Driver Code
 app = tkinterApp()
 app.mainloop()
-
-
-# class render:
-#     def __init__(self) -> None:
-#         """TKinter initialisation"""
-#         tk = Tk()
-#         tk.title("SI-CPE")
-#         tk.geometry('700x500')
-
-#         """Frame TOP"""
-#         FrameTOP = Frame(tk).pack(side='top')
-
-#         #Label de titre
-#         labelHello = Label(FrameTOP, text="Space Invaders the game !", fg='black', font=("Arial",20)).pack(side='top')
-#         """Frame MIDDLE"""
-#         FrameMIDDLE = Frame(tk, bd=5).pack()
-
-#         """Frame BOTTOM"""
-#         FrameBOTTOM = Frame(tk).pack(side='bottom')
-#         #Boutton quit
-#         buttonQuit = Button(FrameBOTTOM, text="QUIT", fg='red', command=tk.destroy).pack(side='bottom')
-        
-#         """NOTHING AFTER THIS LINE"""
-#         tk.mainloop()
-
-if __name__ == "__main__":
-    pass
-    # render()
