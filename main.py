42d5a76eccb24aaca03ae46f7c568bbacdecc376
CSIletonk <raphael.dziopa@gmail.com>
2024-11-04 17:03:07 +0100

    debug

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 import tkinter as tk
 from tkinter import ttk
-  
- 
+from Startpage import StartPage
+from Jeu import Jeu
+from Menu import Menu
+
 LARGEFONT = "Verdana"
   
 class tkinterApp(tk.Tk):
@@ -24,7 +26,7 @@ class tkinterApp(tk.Tk):
         allFrames = [StartPage, Jeu, Menu]
   
         # initializing frames to an empty array
-        self.frames = {}  
+        self.frames = []  
   
         # iterating through a tuple consisting
         # of the different page layouts
@@ -35,69 +37,17 @@ class tkinterApp(tk.Tk):
             # initializing frame of that object from
             # startpage, page1, page2 respectively with 
             # for loop
-            self.frames[F] = frame 
+            self.frames.append(frame)
   
             frame.grid(row = 0, column = 0, sticky ="nsew")
   
-        self.show_frame(StartPage)
+        self.show_frame(0)
   
     # to display the current frame passed as
     # parameter
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+    def show_frame(self, num):
+        frame = self.frames[num]
         frame.tkraise()
-  
-# first window frame startpage
-  
-class StartPage(tk.Frame):
-    def __init__(self, parent, controller): 
-        tk.Frame.__init__(self, parent)
-         
-        label = ttk.Label(self, text ="Welcome to", font = (LARGEFONT,15))
-        label = ttk.Label(self, text ="SPACE INVADERS the Game !", font = (LARGEFONT,25))
-        label.grid(row = 1, column = 1, padx = 10, pady = 10) 
-  
-        button1 = ttk.Button(self, text ="Play",
-        command = lambda : controller.show_frame(Jeu))
-        button1.grid(row = 2, column = 2, padx = 10, pady = 10)
-  
-        button2 = ttk.Button(self, text ="Menu",
-        command = lambda : controller.show_frame(Menu))
-        button2.grid(row = 3, column = 2, padx = 10, pady = 10)
-
-        button3 = ttk.Button(self, text ="QUIT", 
-                             command = lambda : controller.destroy())
-        button3.grid(row = 5, column = 2, padx = 10, pady = 10)         
-  
-class Jeu(tk.Frame):
-    def __init__(self, parent, controller):
-
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Play", font = (LARGEFONT,15))
-        label.grid(row = 1, column = 1, padx = 10, pady = 10)
-        
-        button1 = ttk.Button(self, text ="Menu",
-        command = lambda : controller.show_frame(Menu))
-        button1.grid(row = 2, column = 2, padx = 10, pady = 10)
-
-        button3 = ttk.Button(self, text ="QUIT", 
-                             command = lambda : controller.destroy())
-        button3.grid(row = 5, column = 2, padx = 10, pady = 10)
-  
-class Menu(tk.Frame): 
-    def __init__(self, parent, controller):
-        
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Menu", font = (LARGEFONT,15))
-        label.grid(row = 1, column = 1, padx = 10, pady = 10)
-        
-        button1 = ttk.Button(self, text ="Play",
-        command = lambda : controller.show_frame(Jeu))
-        button1.grid(row = 2, column = 2, padx = 10, pady = 10)
-
-        button3 = ttk.Button(self, text ="QUIT", 
-                             command = lambda : controller.destroy())
-        button3.grid(row = 5, column = 2, padx = 10, pady = 10)
 
 # Driver Code
 app = tkinterApp()
